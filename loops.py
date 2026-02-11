3c655d9725ab4dd74392b868532fe75c69c807ff
Kevin-Palakurthi <kevinpalakurthi@gmail.com>
2026-02-12 00:25:29 +0530

    Add loops example

---
+++ loops.py
@@ -0,0 +1,14 @@
+# Python Loops Example
+
+# For loop
+print("For loop example:")
+for i in range(1, 6):
+    print(i)
+
+# While loop
+print("While loop example:")
+count = 5
+
+while count > 0:
+    print(count)
+    count -= 1
