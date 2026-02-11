f38f11f62919052d7a498c10cd763509a810b292
Kevin-Palakurthi <kevinpalakurthi@gmail.com>
2026-02-12 00:22:19 +0530

    Add conditions example

---
+++ conditions.py
@@ -0,0 +1,20 @@
+# Python Conditions Example
+
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+
+marks = 75
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Grade: Fail")
