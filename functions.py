a8eff3f52a6d0db660e919820b9b1b28b26d9438
Kevin-Palakurthi <kevinpalakurthi@gmail.com>
2026-02-12 00:29:48 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+# Python Functions Example
+
+def greet(name):
+    print("Hello,", name)
+
+greet("Kevin")
+
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(5, 3)
+print("Sum:", result)
